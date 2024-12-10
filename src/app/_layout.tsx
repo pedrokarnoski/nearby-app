6f8761c7dfec1840575c13bd583448942ff7fe9e
Pedro Henrique Karnoski <pedrokarnoski@gmail.com>
2024-12-09 21:09:37 -0300

    feat: create layout

---
+++ src/app/_layout.tsx
@@ -0,0 +1,15 @@
+import { Stack } from "expo-router";
+import {colors} from '@/styles/theme'
+
+export default function Layout() {
+  return (
+    <Stack screenOptions={{
+      headerShown: false,
+      contentStyle: {
+        backgroundColor: colors.gray[100],
+      },
+    }}>
+
+    </Stack>
+  );
+}
